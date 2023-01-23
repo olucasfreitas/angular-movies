49356eb88db277081c0f427806b843f1d8720925
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2023-01-22 23:16:15 -0300

    defined routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { MovieSearchComponent } from './movie-search/movie-search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', title: 'Movies Search', component: MovieSearchComponent },
+  { path: 'favorites', title: 'Favorites', component: FavoritesComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
